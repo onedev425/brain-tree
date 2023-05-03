30d96b866fed73d3db0e5c80efee796bc2da8b21
onedev425 <june.wang914@gmail.com>
2023-05-02 21:06:43 -0400

    update page structure

--- resources/views/components/partials/form-section.blade.php
+++ resources/views/components/partials/form-section.blade.php
@@ -1,11 +1,7 @@
 @props(['submit'])
 
 <div {{ $attributes->merge(['class' => 'md:grid grid-rows-1 grid-cols-12 gap-2']) }}>
-    <div class="col-span-4">
-        <h2 class="text-xl font-bold my-2">{{$title}}</h2>
-        <p class="text-sm">{{$description}}</p>
-    </div>
-    <div class="col-span-8 card shadow-sm">
+    <div class="col-span-12 card shadow-sm">
         <form wire:submit.prevent="{{ $submit }}">
             <div class="card-body">
             {{ $form }}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,17 +26,6 @@
             <div class="lg:flex lg:flex-cols text-gray-900 bg-gray-100 dark:bg-gray-700 dark:text-gray-50 min-h-screen" >
                 <livewire:layouts.menu />
                 <div class="w-full max-w-full overflow-scroll beautify-scrollbar">
-                    <div class="bg-white dark:bg-gray-800 p-4 w-full border-b-2">
-                        <h1 class="text-3xl my-2 capitalize font-semibold">@yield('page_heading')</h1>
-                        <div class="w-full">
-                            <x-show-set-school />
-                        </div>
-                        <div class="w-full">
-                            @isset ($breadcrumbs)
-                                <x-breadcrumbs :paths="$breadcrumbs"/>
-                            @endif
-                        </div>
-                    </div>
                     <main class="p-4" id="main">
                         @yield('content')
                     </main>
