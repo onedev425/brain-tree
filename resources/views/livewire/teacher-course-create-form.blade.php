fc70516b422084e2751773007fa704a5f79e31ac
onedev425 <june.wang914@gmail.com>
2023-05-12 12:55:09 -0400

    add basic code for create a course

--- resources/views/livewire/teacher-course-create-form.blade.php
+++ resources/views/livewire/teacher-course-create-form.blade.php
@@ -1,3 +1,84 @@
 <div>
-    this is the UI for create a course
+    <div class="absolute -mt-16 md:-mt-16 lg:-mt-20 text-center md:text-left w-11/12 md:w-auto">
+        <h1 class="text-xl md:text-3xl capitalize text-white font-semibold">{{__('Course')}}</h1>
+    </div>
+    <div class="block">
+        <div class="float-right my-3">
+            <button type="button" class="py-3 px-11 inline-block text-center mb-3 rounded-lg leading-5 text-red-500 bg-transparent border border-red-500 hover:text-white hover:bg-red-500 hover:ring-0 hover:border-red-500 focus:text-white focus:bg-red-500 focus:border-red-500 focus:outline-none focus:ring-0 mr-4">
+                {{ __('Save') }}
+            </button>
+
+            <button type="button" class="py-3 px-8 inline-block text-center mb-3 rounded-lg leading-5 text-gray-100 bg-red-500 border border-red-500 hover:text-white hover:bg-red-600 hover:ring-0 hover:border-red-600 focus:bg-red-600 focus:border-red-600 focus:outline-none focus:ring-0">
+                {{ __('Publish') }}
+            </button>
+        </div>
+
+        <div class="w-full float-left ">
+            <div class="w-full px-4 pb-4 float-left lg:hidden">
+                <div class="card mt-2">
+                    <div class="card-header mb-7">
+                        <h3 class="text-xl md:text-2xl font-bold">
+                            {{ __('Featured Image') }}
+                        </h3>
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="mb-6">
+                                <label for="course_title" class="block mb-2 font-medium text-gray-900">{{ 'Course Title' }}</label>
+                                <input type="text" id="course_title" class="shadow-sm border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="" required>
+                            </div>
+
+                            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full px-4 pb-4 float-left lg:w-2/3 xl:w-4/5">
+                <div class="card mt-2">
+                    <div class="card-header mb-7">
+                        <h3 class="text-xl md:text-2xl font-bold">
+                            {{ __('Course Basic Info') }}
+                        </h3>
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="mb-6">
+                                <label for="course_title" class="block mb-2 font-medium text-gray-900">{{ 'Course Title' }}</label>
+                                <input type="text" id="course_title" class="shadow-sm border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="" required>
+                            </div>
+
+                            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <div class="hidden ps-4 pb-4 float-left lg:block lg:w-1/3 xl:w-1/5">
+                <div class="card mt-2">
+                    <div class="card-header mb-7">
+                        <h3 class="text-xl md:text-2xl font-bold">
+                            {{ __('Featured Image') }}
+                        </h3>
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="mb-6">
+                                <label for="course_title" class="block mb-2 font-medium text-gray-900">{{ 'Course Title' }}</label>
+                                <input type="text" id="course_title" class="shadow-sm border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="" required>
+                            </div>
+
+                            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+
 </div>
+
+
