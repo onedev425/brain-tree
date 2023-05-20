dcd69da6f1bc9123b1b75cd0888b0701576d398e
onedev425 <june.wang914@gmail.com>
2023-05-20 16:44:58 -0400

    add code for student course page

---
+++ app/Http/Livewire/StudentCourseBlock.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class StudentCourseBlock extends Component
+{
+    public $title;
+    public $image;
+    public $created_by;
+    public $lesson_nums;
+    public $quiz_nums;
+    public $progress;
+
+    public function render()
+    {
+        return view('livewire.student-course-block');
+    }
+}
