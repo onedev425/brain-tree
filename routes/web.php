b4f8678d3dbfd9b5e7b07cd8d7ef6f207bf173a0
onedev425 <june.wang914@gmail.com>
2023-05-20 15:26:11 -0400

    Revert "add code for student course page"
    This reverts commit 9ab5b5c4

--- routes/web.php
+++ routes/web.php
@@ -63,13 +63,6 @@ Route::middleware('auth:sanctum', 'verified', 'App\Http\Middleware\PreventLockAc
     //Marks routes
     Route::get('marks', ['App\Http\Controllers\MarksController', 'index'])->name('marks.index');
 
-    //Students Courses routes
-    Route::prefix('student')->group(function () {
-        Route::resource('course', StudentCourseController::class)->names([
-            'index' => 'student.course.index',
-            'show' => 'student.course.show',
-        ]);
-    });
 });
 
 //user must be authenticated
