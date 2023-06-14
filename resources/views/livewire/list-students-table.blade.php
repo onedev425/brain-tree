45a2ee5dd9c3f7f0a72660e18bd2ecf890e50f6c
onedev425 <june.wang914@gmail.com>
2023-06-15 03:44:37 +0800

    update the code to show student list in teacher student page

--- resources/views/livewire/list-students-table.blade.php
+++ resources/views/livewire/list-students-table.blade.php
@@ -2,8 +2,8 @@
     <div class="card-body">
         @unlessrole(['student'])
         <livewire:datatable :model="App\Models\User::class" uniqueId="students-list-table" :filters="[['name' => 'students'], ['name' => 'orderBy' , 'arguments' => ['name']], ['name' => 'has', 'arguments' => ['StudentRecord']], ['name' => 'with' , 'arguments' => ['studentRecord']]]" :columns="[
-                ['property' => 'name', 'type' => 'href', 'links' => 'students.show', 'text'=> 'view', 'class' => 'text-purple-500', 'image' => 'profile_photo_path'],
-                ['property' => 'created_at', 'name' => ''],
+                ['property' => 'name', 'type' => 'href', 'links' => 'students.show', 'text'=> 'view', 'header_class' => 'text-left font-bold', 'class' => 'text-purple-500', 'image' => 'profile_photo_path'],
+                ['property' => 'created_at', 'name' => 'Enrolleed', 'header_class' => 'text-center font-bold', 'class' => 'text-center'],
             ]
             "/>
         @endhasanyrole
