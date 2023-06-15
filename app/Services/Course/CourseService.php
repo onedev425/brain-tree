891e70bba348e30c31bfd4cc3cf7e1709f36b126
onedev425 <june.wang914@gmail.com>
2023-06-16 00:29:51 +0800

    update the query to get the student's courses

--- app/Services/Course/CourseService.php
+++ app/Services/Course/CourseService.php
@@ -29,13 +29,18 @@ class CourseService
         elseif ($type == 'progress') {
             /*
             SELECT DISTINCT C.* FROM courses C
-            LEFT JOIN lessons L ON C.id = L.course_id
-            LEFT JOIN student_lessons S ON c.id = S.course_id AND S.lesson_id = L.id AND s.student_id = 31
+                INNER JOIN student_courses SC ON C.id = SC.course_id
+                LEFT JOIN lessons L ON C.id = L.course_id
+                LEFT JOIN student_lessons S ON c.id = S.course_id AND S.lesson_id = L.id AND S.student_id = '$student_id'
             WHERE S.course_id IS NULL
             */
 
             $student_id = auth()->user()->id;
             $courses = Course::select('courses.*')
+                ->join('student_courses', function ($join) use ($student_id) {
+                    $join->on('courses.id', '=', 'student_courses.course_id')
+                        ->where('student_courses.student_id', '=', $student_id);
+                })
                 ->leftJoin('lessons', 'courses.id', '=', 'lessons.course_id')
                 ->leftJoin('student_lessons', function ($join) use ($student_id) {
                     $join->on('courses.id', '=', 'student_lessons.course_id')
@@ -51,24 +56,38 @@ class CourseService
         }
         else {
             /*
-            SELECT * FROM courses C WHERE id NOT IN (
+             SELECT * FROM courses C
+             INNER JOIN student_courses SC ON C.id = SC.course_id AND SC.student_id = '$student_id'
+             WHERE SC.course_id NOT IN (
                 SELECT DISTINCT C.id FROM courses C
-                LEFT JOIN lessons L ON C.id = L.course_id
-                LEFT JOIN student_lessons S ON c.id = S.course_id AND S.lesson_id = L.id AND s.student_id = 31
+                    INNER JOIN student_courses SC ON C.id = SC.course_id AND SC.student_id = '$student_id'
+                    LEFT JOIN lessons L ON C.id = L.course_id
+                    LEFT JOIN student_lessons S ON c.id = S.course_id AND S.lesson_id = L.id AND s.student_id = '$student_id'
                 WHERE S.course_id IS NULL
             )
              */
             $student_id = auth()->user()->id;
-            $progress_courses_id = Course::select('courses.id')
-                ->leftJoin('lessons', 'courses.id', '=', 'lessons.course_id')
-                ->leftJoin('student_lessons', function ($join) use ($student_id) {
-                    $join->on('courses.id', '=', 'student_lessons.course_id')
-                        ->on('student_lessons.lesson_id', '=', 'lessons.id')
-                        ->where('student_lessons.student_id', '=', $student_id);
+            $courses = Course::select('courses.*')
+                ->join('student_courses', function ($join) use ($student_id) {
+                    $join->on('courses.id', '=', 'student_courses.course_id')
+                        ->where('student_courses.student_id', '=', $student_id);
                 })
-                ->whereNull('student_lessons.course_id')
-                ->distinct();
-            $courses = Course::whereNotIn('id', $progress_courses_id)->get();
+                ->whereNotIn('courses.id', function ($query) use ($student_id) {
+                    $query->select('courses.id')
+                        ->from('courses')
+                        ->join('student_courses', function ($join) use ($student_id) {
+                            $join->on('courses.id', '=', 'student_courses.course_id')
+                                ->where('student_courses.student_id', '=', $student_id);
+                        })
+                        ->leftJoin('lessons', 'courses.id', '=', 'lessons.course_id')
+                        ->leftJoin('student_lessons', function ($join) use ($student_id) {
+                            $join->on('courses.id', '=', 'student_lessons.course_id')
+                                ->on('lessons.id', '=', 'student_lessons.lesson_id')
+                                ->where('student_lessons.student_id', '=', $student_id);
+                        })
+                        ->whereNull('student_lessons.course_id');
+                })
+                ->get();
         }
 
         return $courses;
@@ -141,6 +160,7 @@ class CourseService
                     SELECT C.title, Q.id, Q.name, Q.points, QO.description, QO.answer, SQ.question_id, SQ.answer student_answer,
                         IF(Q.id = SQ.question_id AND QO.answer = SQ.answer, 1, 0) result
                     FROM courses C
+                    INNER JOIN student_courses SC ON C.id = SC.course_id AND SC.student_id = '$student_id'
                     LEFT JOIN questions Q ON C.id = Q.course_id
                     LEFT JOIN question_options QO ON Q.id = QO.question_id
                     LEFT JOIN student_questions SQ ON C.id = SQ.course_id AND Q.id = SQ.question_id AND QO.id = SQ.question_option_id AND SQ.student_id = '$student_id'
