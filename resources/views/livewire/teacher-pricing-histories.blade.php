6c9d2fe71230868f4ed67f035ff02d55cec10700
onedev425 <june.wang914@gmail.com>
2023-06-22 03:50:49 +0800

    add code to show the sold teacher's course list

--- resources/views/livewire/teacher-pricing-histories.blade.php
+++ resources/views/livewire/teacher-pricing-histories.blade.php
@@ -1,34 +1,44 @@
 <div>
     <x-loading-spinner/>
+    <div class="flex items-center justify-end">
+        <input id="datatable-search-student-purchase-history" type="search" wire:model.sebounce.500ms="search"  placeholder="Search" class="border border-gray-300 rounded-l py-2 px-4"/>
+        <button class="bg-red-500 hover:bg-red-600 text-white rounded-r py-2 px-4 focus:outline-none focus:ring focus:border-red-300">
+            <i class="fas fa-search"></i>
+        </button>
+    </div>
     <div class="overflow-x-scroll beautify-scrollbar text-center my-4 border rounded-lg">
         <table class="w-full table-auto">
             <thead class="">
-            <th class="capitalize p-4 whitespace-nowrap font-normal text-left">Order</th>
-            <th class="capitalize p-4 whitespace-nowrap font-normal text-left">Billing Date</th>
-            <th class="capitalize p-4 whitespace-nowrap font-normal text-left">Amount</th>
+            <th class="capitalize p-4 whitespace-nowrap text-left">{{ __('Course') }}</th>
+            <th class="capitalize p-4 whitespace-nowrap text-left">{{ __('Price') }}</th>
+            <th class="capitalize p-4 whitespace-nowrap text-left">{{ __('Student') }}</th>
+            <th class="capitalize p-4 whitespace-nowrap text-left">{{ __('Purchase At') }}</th>
             </thead>
-            <tbody class="">
-            <tr class="border-t">
-                <td class="p-4 whitespace-nowrap text-left">Wordpress Blogging, Tumblr And Blogger</td>
-                <td class="p-4 whitespace-nowrap text-left">13/5/2023</td>
-                <td class="p-4 whitespace-nowrap text-left">€78.96 EUR</td>
-            </tr>
-            <tr class="border-t">
-                <td class="p-4 whitespace-nowrap text-left">Wordpress Blogging, Tumblr And Blogger</td>
-                <td class="p-4 whitespace-nowrap text-left">13/5/2023</td>
-                <td class="p-4 whitespace-nowrap text-left">€78.96 EUR</td>
-            </tr>
-            <tr class="border-t">
-                <td class="p-4 whitespace-nowrap  text-left">Wordpress Blogging, Tumblr And Blogger</td>
-                <td class="p-4 whitespace-nowrap  text-left">13/5/2023</td>
-                <td class="p-4 whitespace-nowrap  text-left">€78.96 EUR</td>
-            </tr>
-            <tr class="border-t">
-                <td class="p-4 whitespace-nowrap  text-left">Wordpress Blogging, Tumblr And Blogger</td>
-                <td class="p-4 whitespace-nowrap  text-left">13/5/2023</td>
-                <td class="p-4 whitespace-nowrap  text-left">€78.96 EUR</td>
-            </tr>
+            <tbody class="text-left">
+            @if ($courses->isNotEmpty())
+                @foreach($courses as $course)
+                    <tr class="border-t">
+                        <td class="p-4 whitespace-nowrap text-left">
+                            <div class="flex">
+                                <img width="50" height="50" class="rounded-full" src="{{ $course->image }}" onerror="this.src='{{ asset('images/logo/course.jpg') }}'" />
+                                <span class="flex items-center gap-2 py-3 px-6">{{ $course->title }}</span>
+                            </div>
+
+                        </td>
+                        <td class="p-4 whitespace-nowrap">${{ $course->price }}</td>
+                        <td class="p-4 whitespace-nowrap">{{ $course->student_name }}</td>
+                        <td class="p-4 whitespace-nowrap">{{ substr($course->purchase_at, 0, 10) }}</td>
+                    </tr>
+                @endforeach
+            @else
+                <tr>
+                    <td class="p-4 capitalize" colspan="100%">{{ __('No data to show') }}</td>
+                </tr>
+            @endif
             </tbody>
         </table>
+    </div>
+    <div class="my-3">
+        {{ $courses->links() }}
     </div>
 </div>
