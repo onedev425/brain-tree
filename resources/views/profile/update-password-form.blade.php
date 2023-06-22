f6cdd4520a0aa26b4a7afadb2ad7e1b0069d27e4
onedev425 <june.wang914@gmail.com>
2023-06-22 10:50:44 +0800

    update some string

--- resources/views/profile/update-password-form.blade.php
+++ resources/views/profile/update-password-form.blade.php
@@ -16,16 +16,16 @@
                 <div class="w-md-75">
                     <div class="form-group">
                         <x-input id="current_password" type="password"
-                                 wire:model.defer="state.current_password" autocomplete="current-password" name="current_password" label="Current Password"/>
+                                 wire:model.defer="state.current_password" autocomplete="current-password" name="current_password" label="{{ __('Current Password') }}"/>
                     </div>
 
                     <div class="form-group">
-                        <x-input label="New Password" name="password" id="password" type="password"
+                        <x-input label="{{ __('New Password') }}" name="password" id="password" type="password"
                                  wire:model.defer="state.password" autocomplete="new-password" />
                     </div>
 
                     <div class="form-group">
-                        <x-input name="password_confirmation" label="Confirm Password" id="password_confirmation" type="password"
+                        <x-input name="password_confirmation" label="{{ __('Confirm Password') }}" id="password_confirmation" type="password"
                                  wire:model.defer="state.password_confirmation" autocomplete="new-password" />
                     </div>
                 </div>
@@ -33,12 +33,12 @@
             <!-- ... -->
             <div class="sm:mx-3">
                 <div class="bg-gray-200 p-5 rounded-2xl">
-                    Be sure to create a strong and unique password that includes a combination of letters, numbers, and symbols, and avoid using the same password for multiple accounts. If you have any trouble changing your password, don't hesitate to reach out to our support team for assistance.
+                    {{ __('Be sure to create a strong and unique password that includes a combination of letters, numbers, and symbols, and avoid using the same password for multiple accounts. If you have any trouble changing your password, don\'t hesitate to reach out to our support team for assistance.') }}
                     <ul class="mx-5 mt-5 list-disc ">
-                        <li>Minimum 8 character</li>
-                        <li>At least one special character</li>
-                        <li>At least one number</li>
-                        <li>Can't be the same as a  previous</li>
+                        <li>{{ __('Minimum 8 character') }}</li>
+                        <li>{{ __('At least one special character') }}</li>
+                        <li>{{ __('At least one number') }}</li>
+                        <li>{{ __('Can\'t be the same as a  previous') }}</li>
                     </ul>
                 </div>
 
