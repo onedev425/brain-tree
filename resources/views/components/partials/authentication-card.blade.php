617092ee58409ee88948d94238c6956e8f13e61b
onedev425 <june.wang914@gmail.com>
2023-07-24 10:10:40 +0800

    change two factor authentication dialog size

--- resources/views/components/partials/authentication-card.blade.php
+++ resources/views/components/partials/authentication-card.blade.php
@@ -1,7 +1,7 @@
 @props(['class' => ''])
 <div class="flex flex-cols justify-center items-center flex-col">
     <div style="height:250px;background-color:#102E3A;width:100%"></div>
-    <div class="{{$class}} w-10/12 md:w-9/12 lg:w-8/12 xl:w-7/12 min-h-[20%] flex flex-col justify-center items-center">
+    <div class="{{$class}} min-h-[20%] flex flex-col justify-center items-center w-10/12 md:w-9/12 lg:w-8/12 xl:w-7/12 ">
         <div class="w-full bg-white rounded-lg -mt-36 shadow-lg shadow-purple-300 md:shadow-2xl md:shadow-purple-300 ">
             {{$slot}}
         </div>
