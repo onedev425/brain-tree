1cf91c9aa7a123af26cf612023226b085e9d5e62
onedev425 <june.wang914@gmail.com>
2023-07-24 10:11:07 +0800

    fix bug - page expired once login

--- resources/views/livewire/auth-signin-form.blade.php
+++ resources/views/livewire/auth-signin-form.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ route('login') }}" method="POST" class="w-full">
+<form id="login_form" action="{{ route('login') }}" method="POST" class="w-full">
     <div class="block md:flex -mx-3">
         <div class="w-full md:w-1/2 px-3 mb-3">
             <label for="" class="font-medium px-1 block mb-3">{{ __('Email') }} <span class="text-red-500">*</span></label>
@@ -20,10 +20,30 @@
     @csrf
     <div class="flex gap-1 items-center">
         <div class="w-full my-2">
-            <x-button class="mt-3 py-3 md:px-10 text-white font-semibold border-transparent">{{ __('Login account') }}</x-button>
+            <x-button name="login_button" class="mt-3 py-3 md:px-10 text-white font-semibold border-transparent">{{ __('Login account') }}</x-button>
         </div>
     </div>
     <div class="flex gap-1 items-center my-4">
         <a href="{{route('password.request')}}" class="text-black block font-semibold underline" aria-label="Forgot Password">{{ __('Forgot your Password?') }}</a>
     </div>
 </form>
+<script>
+    $('button[name=login_button]').on('click', function(e) {
+        e.preventDefault();
+        $(this).attr('disabled', 'disabled');
+        $.ajax({
+            url: '{{ route('refresh.token') }}',
+            method: 'GET',
+            success: function(response) {
+                console.log(response.token);
+                $('meta[name="csrf-token"]').attr('content', response.token);
+                $('form#login_form input[name=_token]').val(response.token);
+                $('form#login_form').submit();
+            },
+            error: function(xhr, status, error) {
+                // Handle the AJAX error
+            }
+        });
+
+    });
+</script>
