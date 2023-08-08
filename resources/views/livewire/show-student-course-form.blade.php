f0bfa688c6ba26e6adfaae263ce8968a5855aa99
onedev425 <june.wang914@gmail.com>
2023-08-07 22:00:40 -0400

    update code if a course completed

--- resources/views/livewire/show-student-course-form.blade.php
+++ resources/views/livewire/show-student-course-form.blade.php
@@ -223,13 +223,13 @@
 
                 </div>
 
-                <div id="quiz_section" class="hidden col-span-7 p-6>
+                <div id="quiz_section" class="hidden col-span-7 p-6">
                     <h1 class="text-xl font-bold mb-4">{{ __('Final Quiz') }}</h1>
                     <div id="quiz_title" class="my-5"></div>
                     <div id="quiz_description" class="my-5 text-gray-400"></div>
                     <div id="question_list"></div>
 
-                    <div class="flex justify-end mt-10 {{ $passed_exam ? 'hidden' : '' }}"">
+                    <div class="flex justify-end mt-10 {{ $passed_exam ? 'hidden' : '' }}">
                         <h2 class="font-bold mr-3 text-lg">{{ __('Next') }}</h2>
                         <a id="next_quiz" href="javascript:;">
                             <svg xmlns="http://www.w3.org/2000/svg" width="32.975" height="24.718" viewBox="0 0 32.975 24.718">
