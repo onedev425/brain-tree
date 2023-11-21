df6b513edd13924d5d0a6a5931b3abbc05ef775b
onedev425 <june.wang914@gmail.com>
2023-11-21 10:55:53 +0900

    Added avatar to top

--- resources/views/livewire/layouts/header.blade.php
+++ resources/views/livewire/layouts/header.blade.php
@@ -6,13 +6,30 @@
 		<a href="https://braintreespro.com/home/services/" class="p-3 hidden md:flex md:p-5">SERVICES</a>
 		<a href="https://braintreespro.com/home/blogs" class="p-3 hidden md:flex md:p-5">BLOGS</a>
 		<a href="https://braintreespro.com/home/contact-us" class="p-3 hidden md:flex md:p-5">CONTACT</a>
+
 		@if(auth()->user())
-			<form id="logout_form" action="{{route('logout')}}" class="p-3 ml-auto md:ml-0 md:p-5 mb-0" method="POST">
-					@csrf
-					<button name="logout_button" class="text-dark gap-2 text-center">
-						{{ __('LOGOUT') }}
-					</button>
-			</form>
+			@php( $photo_path = auth()->user()->profile_photo_path)
+			<div x-data="{ open: false }" class="relative mr-5">
+				<div @click="open = !open" class="flex items-center cursor-pointer">
+					<div class="rounded-full overflow-hidden mr-2">
+						{{ $photo_path }}
+						<img src="{{ $photo_path == '' ? '/images/logo/avatar.png' : $photo_path }}" alt="Avatar" class="w-8 h-8 object-cover">
+					</div>
+					<span>{{ auth()->user()->name }}</span>
+				</div>
+
+				<div x-show="open" @click.away="open = false" class="absolute z-50 mt-2 right-px w-32 bg-white border rounded shadow-lg">
+					<!-- Dropdown items go here -->
+					<a href="/dashboard" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">{{ __('Profile') }}</a>
+					<a href="/settings" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">{{ __('Settings') }}</a>
+					<form id="logout_form" action="{{route('logout')}}" class="block px-4 py-2" method="POST">
+							@csrf
+							<button name="logout_button" class="text-gray-800 hover:bg-gray-200">
+								{{ __('Logout') }}
+							</button>
+					</form>
+				</div>
+			</div>
 		@endif
 	</div>
 	@if(auth()->user())
